cffeae96c205d12d5a494158270e2a7acc871889
Jacques Spykerman <jacwil747@gmail.com>
2025-07-18 09:30:56 +0000

    Still not fixed

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -188,7 +188,7 @@ export default function AdminLayout({
     <SidebarProvider defaultOpen>
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
             <AdminSidebar />
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 transition-[padding-left] group-data-[state=expanded]/sidebar-wrapper:sm:pl-64">
+            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-[--sidebar-width-icon] group-data-[state=expanded]/sidebar-wrapper:sm:pl-[--sidebar-width] transition-[padding-left]">
                 <AdminHeader />
                 <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
                     {children}
