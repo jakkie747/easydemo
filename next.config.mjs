17f878d4fecb49d7351eabe11944e33c5d657e1e
Jacques Spykerman <jacwil747@gmail.com>
2025-07-19 08:56:33 +0000

    I am here now

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Your Next.js configuration options...
+  reactStrictMode: true,
 };
 
 export default nextConfig;
