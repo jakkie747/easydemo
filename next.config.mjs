1db130ac9b3abfb4f4ef8747b443ec0ec015bc07
Jacques Spykerman <jacwil747@gmail.com>
2025-07-19 08:47:22 +0000

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Your Next.js configuration options go here.
+  // For example, to add a remote pattern for images:
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'example.com',
+  //       port: '',
+  //       pathname: '/images/**',
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
