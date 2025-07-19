9b9742fe5a4789a8ea86a1d075cb9641cf719d9b
Jacques Spykerman <jacwil747@gmail.com>
2025-07-19 19:56:28 +0000

    Error: Module not found: Error: Can't resolve 'next-client-pages-loader'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
