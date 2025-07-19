281c533dbd09173edfbc6438e8d0a4a6178d188e
Jacques Spykerman <jacwil747@gmail.com>
2025-07-19 04:31:49 +0000

    I have started the build and got this error - starting build "5e975967-f

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 
-import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
--- next.config.ts
+++
@@ -1,5 +0,0 @@
-
-// This file is deprecated. Please use src/next.config.ts instead.
-import nextConfig from './src/next.config';
-
-export default nextConfig;
