485ec0ddb6b3368fad4dd2cfbb72ad93969fe951
Jacques Spykerman <jacwil747@gmail.com>
2025-07-19 06:19:56 +0000

    In github I can't find the file page.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      }
-    ],
-  },
 };
 
 export default nextConfig;
