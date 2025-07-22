1d348769747a0682fe3fc198878411d91214827c
Jacques Spykerman <jacwil747@gmail.com>
2025-07-22 11:09:37 +0000

    https://nextjs.org/telemetry
      ▲ Next.js 14.2.30
    
       Creating an optimi

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,8 @@
 
-import type { SVGProps } from "react";
+import type { ComponentProps } from "react";
 import Image from "next/image";
 
-export function Logo(props: Omit<SVGProps<SVGSVGElement>, 'children' | 'width' | 'height'> & {width?: number, height?: number}) {
+export function Logo(props: Omit<ComponentProps<typeof Image>, 'src' | 'alt' >) {
   const { width = 56, height = 56, ...rest } = props;
   return (
     <Image src="/logo.png" alt="Easyspark Logo" width={width} height={height} {...rest} />
