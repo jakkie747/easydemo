6577d445e694a782e49bc863df0c1355762c6acc
Jacques Spykerman <jacwil747@gmail.com>
2025-07-24 13:54:35 +0000

    Updates to app

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,12 +3,13 @@
 
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
+import { firebase } from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
     }),
+    firebase(),
   ],
-  enableTracingAndMetrics: true,
 });
